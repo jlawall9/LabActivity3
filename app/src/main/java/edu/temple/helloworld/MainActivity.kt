ec7ebe4dc1f89343a5dea57646d27d3099691b28
tun19502 <johnlawall24@gmail.com>
2024-09-25 16:11:25 -0400

    finisheed

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
                 displayTextView.text = "Please enter your name"
             } else {
                 displayTextView.text = "Hello, $name"
-                //added changessss
+                //added changesssssss
             }
         }
 
